9282908ee340fbf9aea0908973765bd3150db977
Pranjal Das <pranjaldas525@gmail.com>
2020-03-18 12:06:28 +0530

    Personaldetails data stored in database successfully

---
+++ src/main/java/com/doat/recruitment/jpa/services/AddressService.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020. All rights reserved to Directorate Of accounts and Treasuries, Guwahati.
+ * @Author: Pranjal Das
+ */
+
+package com.doat.recruitment.jpa.services;
+
+public class AddressService {
+}
